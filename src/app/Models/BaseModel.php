8bdd018fb1ea386a13ce958ceca3c2f29c995ad0
B1nj <postmaster@b1nj.fr>
2022-07-28 15:56:26 -0400

    phpdoc models generate

--- src/app/Models/BaseModel.php
+++ src/app/Models/BaseModel.php
@@ -4,6 +4,14 @@ namespace Ipsum\Core\app\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Ipsum\Core\app\Models\BaseModel
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|BaseModel newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BaseModel newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BaseModel query()
+ * @mixin \Eloquent
+ */
 class BaseModel extends Model
 {
 
--- src/app/Models/Setting.php
+++ src/app/Models/Setting.php
@@ -3,6 +3,25 @@
 namespace Ipsum\Core\app\Models;
 
 
+/**
+ * Ipsum\Core\app\Models\Setting
+ *
+ * @property int $id
+ * @property string $group
+ * @property string $key
+ * @property string $name
+ * @property string|null $description
+ * @property string|null $value
+ * @property string $type
+ * @property array|null $options
+ * @property string|null $rules
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting query()
+ * @mixin \Eloquent
+ */
 class Setting extends BaseModel
 {
     protected $table = 'settings';
